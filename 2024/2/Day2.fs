c0746244058411089783abe991db0798e938e645
Mikael Kajsjö <mkajsjo@gmail.com>
2024-12-06 01:55:15 +0100

    change day 2

--- 2024/2/Day2.fs
+++ 2024/2/Day2.fs
@@ -2,11 +2,13 @@ module Day2
 
 let isSafe row =
     let pairs = row |> Seq.pairwise
-    let diffs = pairs |> Seq.map (fun (a, b) -> abs (a - b))
-    let ascending = pairs |> Seq.forall (fun (a, b) -> a > b)
-    let descending = pairs |> Seq.forall (fun (a, b) -> a < b)
-    let diffReq = diffs |> Seq.forall (fun d -> d >= 1 && d <= 3)
-    (ascending || descending) && diffReq
+    let all pred = pairs |> Seq.forall (fun (a, b) -> pred a b)
+
+    (all (>) || all (<)) &&
+    all (fun a b ->
+        let diff = abs (a - b)
+        diff >= 1 && diff <= 3
+    )
 
 let dampener row =
     [0..Seq.length row - 1]
@@ -14,14 +16,11 @@ let dampener row =
         row |> Seq.indexed |> Seq.filter (fst >> (<>) i) |> Seq.map snd
     )
 
-let solve1 (input: array<string>) =
+let countReports predicate (input: array<string>) =
     input
     |> Seq.map (fun s -> s.Split(' ') |> Seq.map int)
-    |> Seq.filter isSafe
+    |> Seq.filter predicate
     |> Seq.length
 
-let solve2 (input: array<string>) =
-    input
-    |> Seq.map (fun s -> s.Split(' ') |> Seq.map int)
-    |> Seq.filter (dampener >> Seq.exists isSafe)
-    |> Seq.length
+let solve1 = countReports isSafe
+let solve2 = countReports (dampener >> Seq.exists isSafe)
