05d9acd3cdcf15f6b2826d2ce5454be77e372933
Mikael Kajsjö <mkajsjo@gmail.com>
2024-12-06 01:43:46 +0100

    day 2

--- 2024/2/Day2.fs
+++ 2024/2/Day2.fs
@@ -1,7 +1,27 @@
 module Day2
 
+let isSafe row =
+    let pairs = row |> Seq.pairwise
+    let diffs = pairs |> Seq.map (fun (a, b) -> abs (a - b))
+    let ascending = pairs |> Seq.forall (fun (a, b) -> a > b)
+    let descending = pairs |> Seq.forall (fun (a, b) -> a < b)
+    let diffReq = diffs |> Seq.forall (fun d -> d >= 1 && d <= 3)
+    (ascending || descending) && diffReq
+
+let dampener row =
+    [0..Seq.length row - 1]
+    |> Seq.map (fun i ->
+        row |> Seq.indexed |> Seq.filter (fst >> (<>) i) |> Seq.map snd
+    )
+
 let solve1 (input: array<string>) =
-    failwith "TODO"
+    input
+    |> Seq.map (fun s -> s.Split(' ') |> Seq.map int)
+    |> Seq.filter isSafe
+    |> Seq.length
 
 let solve2 (input: array<string>) =
-    failwith "TODO"
+    input
+    |> Seq.map (fun s -> s.Split(' ') |> Seq.map int)
+    |> Seq.filter (dampener >> Seq.exists isSafe)
+    |> Seq.length
