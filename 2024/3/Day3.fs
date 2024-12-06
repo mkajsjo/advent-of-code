30450bf36bd0b16d234c872d15479ad0b365cf1b
Mikael Kajsjö <mkajsjo@gmail.com>
2024-12-06 02:13:52 +0100

    day 3

--- 2024/3/Day3.fs
+++ 2024/3/Day3.fs
@@ -1,7 +1,31 @@
 module Day3
 
+open System.Text.RegularExpressions
+
+let mulGroups (m: Match) = int m.Groups[1].Value * int m.Groups[2].Value
+
 let solve1 (input: array<string>) =
-    failwith "TODO"
+    input
+    |> Seq.collect (fun row -> Regex.Matches (row, @"mul\((\d+),(\d+)\)"))
+    |> Seq.sumBy mulGroups
 
 let solve2 (input: array<string>) =
-    failwith "TODO"
+    let mutable enabled = true
+
+    input
+    |> Seq.collect (fun row -> Regex.Matches (row, @"mul\((\d+),(\d+)\)|(do|don't)\(\)"))
+    |> Seq.sumBy (fun m ->
+        match m.Groups[3].Value with
+        | "do" ->
+            enabled <- true
+            0
+        | "don't" ->
+            enabled <- false
+            0
+        | _ ->
+            match enabled with
+            | true -> mulGroups m
+            | false -> 0
+    )
+
+
