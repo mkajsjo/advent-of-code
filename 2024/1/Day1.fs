029ebbbdc561ed98b237474c3e9fb9e75b35cade
Mikael Kajsjö <mkajsjo@gmail.com>
2024-12-06 01:09:30 +0100

    day 1

--- 2024/1/Day1.fs
+++ 2024/1/Day1.fs
@@ -1,7 +1,17 @@
 module Day1
 
 let solve1 (input: array<string>) =
-    failwith "TODO"
+    input
+    |> Array.map (fun s -> s.Split("   ") |> Array.map int)
+    |> Array.transpose
+    |> Array.map Array.sort
+    |> Array.transpose
+    |> Array.sumBy (fun rows -> abs (rows[0] - rows[1]))
 
 let solve2 (input: array<string>) =
-    failwith "TODO"
+    input
+    |> Array.map (fun s -> s.Split("   ") |> Array.map int)
+    |> Array.transpose
+    |> fun columns ->
+        let nrCounts = columns[1] |> Seq.countBy id |> Map.ofSeq
+        columns[0] |> Array.sumBy (fun n -> n * (Map.tryFind n nrCounts |> Option.defaultValue 0))
