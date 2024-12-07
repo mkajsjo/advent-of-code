3ca120e911fa68d1cc025bce546ab889b659346f
= <mkajsjo@gmail.com>
2024-12-07 22:13:00 +0100

    day 4

--- 2015/4/Day4.fs
+++ 2015/4/Day4.fs
@@ -1,5 +1,20 @@
 module Day4
 
-let solve1 (input: array<string>) = 0
+let md5 (str: string) =
+    use md5 = System.Security.Cryptography.MD5.Create()
 
-let solve2 (input: array<string>) = 0
+    str
+    |> System.Text.Encoding.ASCII.GetBytes
+    |> md5.ComputeHash
+    |> Seq.map (fun c -> c.ToString("X2"))
+    |> Seq.reduce (+)
+
+let findHash (n: int) (input: array<string>) =
+    let secret = input |> Seq.head
+    let zeros = System.String('0', n)
+
+    Seq.initInfinite id
+    |> Seq.findIndex (fun i -> (md5 $"{secret}{i}")[0 .. n - 1] = zeros)
+
+let solve1 (input: array<string>) = findHash 5 input
+let solve2 (input: array<string>) = findHash 6 input
