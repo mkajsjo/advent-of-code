6cc58180fbaf7754f78ebc74280b55ceab561559
= <mkajsjo@gmail.com>
2024-12-07 22:21:56 +0100

    day 5

--- 2015/5/Day5.fs
+++ 2015/5/Day5.fs
@@ -1,5 +1,16 @@
 module Day5
 
-let solve1 (input: array<string>) = 0
+open System.Text.RegularExpressions
 
-let solve2 (input: array<string>) = 0
+let solve1 (input: array<string>) =
+    input
+    |> Seq.filter (fun row ->
+        Regex.Matches(row, "[aoeui]") |> Seq.length >= 3
+        && (Regex.Match(row, "(.)\1")).Success
+        && not (Regex.Match(row, "ab|cd|pq|xy")).Success)
+    |> Seq.length
+
+let solve2 (input: array<string>) =
+    input
+    |> Seq.filter (fun row -> (Regex.Match(row, "(.{2}).*\1")).Success && (Regex.Match(row, "(.).\1")).Success)
+    |> Seq.length
