db686810f0cb5ac4904dcb15002df96a094b87df
= <mkajsjo@gmail.com>
2024-12-07 21:43:28 +0100

    day 2

--- 2015/2/Day2.fs
+++ 2015/2/Day2.fs
@@ -1,5 +1,23 @@
 module Day2
 
-let solve1 (input: array<string>) = 0
+open System.Text.RegularExpressions
 
-let solve2 (input: array<string>) = 0
+let parse (input: array<string>) =
+    input
+    |> Seq.map (fun row ->
+        let m = Regex.Match(row, @"(\d+)x(\d+)x(\d+)")
+        int m.Groups[1].Value, int m.Groups[2].Value, int m.Groups[3].Value)
+
+let solve1 (input: array<string>) =
+    input
+    |> parse
+    |> Seq.sumBy (fun (l, w, h) ->
+        let a, b, c = l * w, w * h, h * l
+        2 * (a + b + c) + min a (min b c))
+
+let solve2 (input: array<string>) =
+    input
+    |> parse
+    |> Seq.sumBy (fun (l, w, h) ->
+        let a, b, c = l + w, w + h, h + l
+        2 * min a (min b c) + l * w * h)
